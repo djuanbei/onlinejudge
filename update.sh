d3a820686b6a3665d23c14261de94287fd87a441
yun <dlyun2009@gmail.com>
2019-09-22 19:26:47 +0800

    ok

--- update.sh
+++ update.sh
@@ -1,17 +1,11 @@
 
-
 #!/bin/bash
-if [ ! $GIT_PREVIOUS_SUCCESSFUL_COMMIT ];then
-    echo "GIT_PREVIOUS_SUCCESSFUL_COMMIT is not exists."
-    exit 0
+
+result=$(git status)
+
+if echo "$result" | grep -q "ahead of 'origin/master' by"; then
+  echo "matched";
 else
-    echo "GIT_COMMIT=[$GIT_COMMIT],GIT_PREVIOUS_SUCCESSFUL_COMMIT=[$GIT_PREVIOUS_SUCCESSFUL_COMMIT]"
-    if [ $GIT_PREVIOUS_SUCCESSFUL_COMMIT == $GIT_COMMIT ];then
-        echo "GIT_COMMIT is equals to GIT_PREVIOUS_SUCCESSFUL_COMMIT,skip build."
-        exit -1
-    else
-        echo "GIT_COMMIT is not equals to GIT_PREVIOUS_SUCCESSFUL_COMMIT"
-        sh build_using_xctool.sh//这里是有代码更新的条件下需要执行的代码，我这里是跑一个脚本
-        exit 0
-    fi
+  echo "no match";
 fi
+
